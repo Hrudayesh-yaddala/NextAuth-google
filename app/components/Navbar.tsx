075840b7d75e3c0b4e3e2eb041e8e3601fbfd723
Hrudayesh <hrudayesh22@gmail.com>
2024-11-07 12:55:40 +0530

    feat : Initial navbar created

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export default function Navbar() {
+  const session = useSession();
+  return (
+    <div className="flex justify-between bg-green-600 h-12 ">
+      <div className=" font-bold mx-4 flex items-center">Logo</div>
+      <div>
+        {session.data?.user ? (
+          <button
+            className="bg-red-500 p-2 font-semibold rounded-md"
+            onClick={() => signOut()}
+          >
+            Sign Out
+          </button>
+        ) : (
+          <button
+            className="bg-sky-400 p-2 font-semibold rounded-md"
+            onClick={() => signIn()}
+          >
+            Sign In
+          </button>
+        )}
+      </div>
+    </div>
+  );
+}
